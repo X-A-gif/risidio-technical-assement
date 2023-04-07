bf45ac956be03176fcc1d8a8e65c8aeebc95cd2e
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-04-07 12:35:57 -0700

    banner head

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import { AiOutlinePicture } from 'react-icons/ai';
+import React, { useState, useEffect } from 'react'
 
-import logo from '../assets/banner.jpg'
+import banner from '../assets/pollock(1).jpg'
+import icon from '../assets/icon.png'
 
 const Hero = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const heroHeadStyles = {
+    fontSize: screenWidth >= 1070 ? '64px' : '32px',
+    color: 'white',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 10,
+  };
+
   return (
     <div>
-    <header>
-      <div className='relative'>
-        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
-          <AiOutlinePicture size={50} />
+      <header>
+        <div className='relative'>
+          <div>
+            <h1 style={heroHeadStyles}>Collection Name</h1>
+            <img className='banner w-full' src={banner}/>
+          </div>
+          <img className='ml-auto mr-auto -mt-10' src={icon} alt="" />
         </div>
-      </div>
-    </header>
-  </div>
+      </header>
+    
+    </div>
   )
 }
 
